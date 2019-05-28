556ccecff7d3b57f24ad534ce3e2dbcc4896a10b
Selvin Norman <selvin715@gmail.com>
2019-05-28 15:53:45 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,65 +1,60 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import Amplify from 'aws-amplify';
-Amplify.configure(aws_exports);
-import aws_exports from './src/aws-exports';
-import AppNavigator from './navigation/AppNavigator';
+import { Linking, Button, StyleSheet, Text, View } from 'react-native';
+import Auth from '@aws-amplify/auth';
+import Analytics from '@aws-amplify/analytics';
+
+import awsconfig from './aws-exports';
+
+// retrieve temporary AWS credentials and sign requests
+Auth.configure(awsconfig);
+// send analytics events to Amazon Pinpoint
+Analytics.configure(awsconfig);
 
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
+    constructor(props) {
+      super(props);
+      this.handleAnalyticsClick = this.handleAnalyticsClick.bind(this);
+      this.state = {resultHtml: <Text></Text>, eventsSent: 0};
+    }
 
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
+    handleAnalyticsClick() {
+      Analytics.record('AWS Amplify Tutorial Event')
+        .then( (evt) => {
+            const url = 'https://'+awsconfig.aws_project_region+'.console.aws.amazon.com/pinpoint/home/?region='+awsconfig.aws_project_region+'#/apps/'+awsconfig.aws_mobile_analytics_app_id+'/analytics/events';
+            let result = (
+              <View>
+                <Text>Event Submitted.</Text>
+                <Text>Events sent: {++this.state.eventsSent}</Text>
+                <Text style={styles.link} onPress={() => Linking.openURL(url)}>
+                  View Events on the Amazon Pinpoint Console
+                </Text>
+              </View>
+            );
+            this.setState({
+                'resultHtml': result
+            });
+        });
+    };
+
+    render() {
       return (
         <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+          <Text>Welcome to your React Native App with Amplify!</Text>
+          <Button title="Generate Analytics Event" onPress={this.handleAnalyticsClick} />
+          {this.state.resultHtml}
         </View>
       );
     }
-  }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
-
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  link: {
+    color: 'blue'
+  }
 });
